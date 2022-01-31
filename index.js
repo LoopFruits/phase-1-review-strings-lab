436b0dd0e0b2e6e75ae852e7f5608e979aaf09f8
Kenneth Jordan <kensworldc@gmail.com>
2022-01-31 11:23:39 -0500

    Done.

--- index.js
+++ index.js
@@ -1,8 +1,28 @@
 // Write your code in this file!
-const currentUser = `Grace Hopper`;
+const currentUser = 'Grace Hopper';
 
 const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
 
 const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+//we set excitedWelcomeMessage equal to the statement in welcomeMessage which turns out to be upper case because of the .toUpperCase
 
-const shortGreeting = `Welcome,  ${currentUser.slice(0,1)}!`;
+
+'Jimmy Jon'[0];
+// => J 
+
+'Jimmy Jon'.length;
+// => 9
+
+'Jimmy Jon'[9];
+// => n
+
+
+'Jimmy Jon'.slice();
+// Jimmy Jon     if the parenthesis are em,py here we get a full copy of the original string
+
+'Jimmy Jon'.slice(6);
+// => Jon
+
+
+const shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!`;
+// allows us to take the first and second elements we stored, being welcome to flat book and grace hopper
